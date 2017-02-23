ce2dc8076b1e54e1d7233eb6dcd1396a809f159f
Sandesh Meda <sandeshmeda@gmail.com>
2017-02-23 16:37:09 -0600

    Initial commit of the TestConsole project

---
+++ example/Serilog.Sinks.Rollbar.TestConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serilog.Sinks.Rollbar.TestConsole
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var log = new LoggerConfiguration()
+                   .MinimumLevel.Verbose()
+                   .WriteTo.RollbarSink("apikey", "dev")
+                   .CreateLogger();
+
+      try
+      {
+        log.Verbose("This is a verbose message!");
+        log.Debug("This is a debug message!");
+        log.Information("This is an information message!");
+        log.Warning("This is a warning message!");
+        log.Error("This is an error message!");
+        throw new Exception("This is an exception!");
+      }
+      catch (Exception exception)
+      {
+        log.Fatal(exception, "Exception caught in Main.");
+      }
+    }
+  }
+}
